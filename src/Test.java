58a6a2e7305e0bcc9bc2ba6f7e6d4757d61cbf85
PratsJadhav <38518773+PratsJadhav@users.noreply.github.com>
2018-05-14 23:36:50 +0530

    jhdg
    jdf

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,12 @@ public interface Test{
 	
 	public void fg();
 	
-	public void ssa();
+	
 	
 	public void gd();
+	
+	public default void test()
+	{
+	System.out.println("testing default...");
+	}
 }
